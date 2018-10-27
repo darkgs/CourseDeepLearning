fc0d007b1e5a27d9636492af7acefabe8fda29d3
bonhun koo <darkgs@snu.ac.kr>
2018-10-27 19:03:14 +0900

    train!

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -375,8 +375,8 @@ def part_3(gpu_num):
     elif gpu_num == '2':
         args.save_dir = 'models_char_rnn_{}'.format(gpu_num)
         args.init_from = args.save_dir
-        args.rnn_size = 396
-        args.num_layers = 3
+        args.rnn_size = 486
+        args.num_layers = 2
         args.decay_rate = 0.1
         args.output_keep_prob = 0.1
         args.input_keep_prob = 0.1
@@ -385,12 +385,12 @@ def part_3(gpu_num):
     elif gpu_num == '3':
         args.save_dir = 'models_char_rnn_{}'.format(gpu_num)
         args.init_from = args.save_dir
-        args.rnn_size = 396
-        args.num_layers = 3
+        args.rnn_size = 1138
+        args.num_layers = 1
         args.decay_rate = 0.1
         args.output_keep_prob = 0.1
         args.input_keep_prob = 0.1
-        args.grad_clip = 5.0
+        args.grad_clip = 20.0
         args.num_epochs = 500
 
     # protip: always check the data and poke around the data yourself
